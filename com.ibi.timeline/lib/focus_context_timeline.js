27cff8eab86c9cb084d5074ee1f89a37a1324c02
Sergey Zakharchenko <sergzak022@gmail.com>
2016-09-14 22:52:19 -0700

    [com.ibi.timeline] added support for new chart properties

--- com.ibi.timeline/lib/focus_context_timeline.js
+++ com.ibi.timeline/lib/focus_context_timeline.js
@@ -350,7 +350,7 @@
                 start = x( parseDateStr(d.start) );
                 end = x( parseDateStr(d.end) );
                 return ( end - start ) > 0;
-            }).map(function(d) {
+            }).map(function(d, i) {
                 start = x( parseDateStr(d.start) );
                 end = x( parseDateStr(d.end) );
 
@@ -360,7 +360,9 @@
                     y: y(d.task) + riserVertOffset,
                     width: end - start,
                     height: riserHeight,
-                    fill: colors[hash(d.task) % colors.length]
+                    fill: (props.colorByRow)
+                      ? colors[hash(d.task) % colors.length]
+                      : colors[i % colors.length]
                 };
                 // subtask string is used as a label
                 var lblDim = props.measureLabel(d.subtask || d.task, props.risers.labels.font);
@@ -498,16 +500,40 @@
                         riser.attr('tdgtitle', getTDGTitleHTML(d.tooltip) );
                     }
 
+                })
+                .style({
+                  'stroke': props.risers.borderColor,
+                  'stroke-width': props.risers.borderWidth
                 });
 
-            if (!props.isInteractionDisabled) {
-              riser_lbl_groups_enter.on('mouseover', function () {
-                  d3.select(this).select('rect').style('stroke', 'black');
-                  canvas_group.node().appendChild(d3.select(this).node());
+                function fade(opacity, selection) {
+                    return function(d, i) {
+                        var fadeout = selection.filter(function(d, idx) {
+                            return (d.idx) ? d.idx !== i : idx !== i;
+                        });
 
-              }).on('mouseout', function () {
-                  d3.select(this).select('rect').style('stroke', null);
-              });
+                        // if ( d3.event.type === 'touchstart' ) {
+                        //   selection.filter(function() {
+                        //       return fadeout[0].indexOf(this) < 0;
+                        //   })
+                        //   .transition()
+                        //   .style('opacity', 1);
+                        // }
+
+                        fadeout.transition().style('opacity', opacity);
+                    };
+                }
+
+            if (!props.isInteractionDisabled && !props.context.enabled) {
+              riser_lbl_groups_enter
+                .on('mouseover', fade(0.3, riser_lbl_groups_enter))
+                .on('mouseout', fade(1, riser_lbl_groups_enter));
+                // .on('mouseover', function () {
+                //     d3.select(this).select('rect').style('stroke', 'black');
+                //     canvas_group.node().appendChild(d3.select(this).node());
+                // }).on('mouseout', function () {
+                //     d3.select(this).select('rect').style('stroke', null);
+                // });
             }
 
             riser_lbl_groups_enter.filter(function (d) {
@@ -558,6 +584,7 @@
                 height: 400,
                 data: null,
                 measureLabel: null,
+                colorByRow: false,
                 onRenderComplete: function() {},
                 isInteractionDisabled: false,
                 context: {
@@ -590,6 +617,8 @@
                         font: '12px sans-serif',
                         color: 'black'
                     },
+                    borderColor: 'white',
+                    borderWidth: 2,
                     tdgtitle: {
                         enabled: false
                     }
@@ -598,7 +627,7 @@
 
             var innerProps = {
                 timeAxisScaleDomain: null,
-                colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"],
+                colors: ["#4087b8", "#e31a1c", "#9ebcda", "#c994c7", "#41b6c4", "#49006a", "#ec7014", "#a6bddb", "#67001f", "#800026", "#addd8e", "#e0ecf4", "#fcc5c0", "#238b45", "#081d58", "#d4b9da", "#2b8cbe", "#74a9cf", "#41ab5d", "#fed976", "#ce1256", "#7f0000", "#a6bddb", "#ffffcc", "#e7e1ef", "#016c59", "#f7fcfd", "#99d8c9", "#fff7fb", "#ffffe5", "#fdd49e", "#ffffd9", "#fe9929", "#8c96c6", "#810f7c", "#993404", "#c7e9b4", "#bfd3e6", "#e7298a", "#7fcdbb", "#3690c0", "#ae017e", "#d9f0a3", "#ece2f0", "#014636", "#f7fcb9", "#66c2a4", "#fff7bc", "#f7fcf0", "#e5f5f9", "#fdbb84", "#fa9fb5", "#4d004b", "#fff7fb", "#cc4c02", "#78c679", "#1d91c0", "#ccebc5", "#feb24c", "#b30000", "#8c6bb1", "#fec44f", "#d0d1e6", "#084081", "#0868ac", "#f7fcfd", "#0570b0", "#ef6548", "#fff7ec", "#006837", "#f768a1", "#edf8b1", "#fee391", "#238443", "#ffffe5", "#023858", "#7a0177", "#67a9cf", "#dd3497", "#980043", "#88419d", "#d0d1e6", "#fc8d59", "#4eb3d3", "#fd8d3c", "#fff7f3", "#fc4e2a", "#ccece6", "#ece7f2", "#a8ddb5", "#41ae76", "#bd0026", "#e0f3db", "#045a8d", "#ffeda0", "#253494", "#7bccc4", "#fde0dd", "#00441b", "#225ea8", "#006d2c", "#02818a", "#f7f4f9", "#d7301f", "#df65b0", "#662506", "#3690c0", "#004529", "#fee8c8"],
                 selection: null // this attr get value after the first draw
             };
 
@@ -756,6 +785,7 @@
             focusRisersProps.tdgtitle.enabled = true;*/
 
             var focus_chart = tdg_timeline({
+                colorByRow: props.colorByRow,
                 width: layout.focus.width,
                 height: layout.focus.height,
                 data: props.data,
@@ -788,6 +818,7 @@
             }
 
             var context_chart = tdg_timeline({
+                colorByRow: props.colorByRow,
                 width: layout.context.width,
                 height: layout.context.height,
                 data: props.data,
@@ -817,6 +848,7 @@
         // --------------------------------- END OF Z1
         return function(user_props) {
             var props = {
+                colorByRow: false,
                 width: 400,
                 height: 300,
                 data: null,
@@ -846,7 +878,9 @@
                         labels: {
                             font: '12px sans-serif',
                             color: 'black'
-                        }
+                        },
+                        borderColor: 'white',
+                        borderWidth: 5
                     }
                 },
                 context: {
@@ -867,7 +901,9 @@
                         labels: {
                             font: '10px sans-serif',
                             color: 'black'
-                        }
+                        },
+                        borderColor: 'white',
+                        borderWidth: 5
                     }
                 }
             };
