f71d1ea0173d2fb10e4ceab9ef7a1c9836f0b5b5
galoaragoneses <galo.aragoneses@gmail.com>
2019-02-13 10:22:48 +0100

    Update jQuery path

--- com.ibi.horizontal_comparison_bars/com.ibi.horizontal_comparison_bars.js
+++ com.ibi.horizontal_comparison_bars/com.ibi.horizontal_comparison_bars.js
@@ -72,13 +72,20 @@
 		noDataRenderCallback: noDataRenderCallback,
 		resources: {  // Additional external resources (CSS & JS) required by this extension
 			script: (function() {
-				var scripts = window.jQuery ? [] : [ ['', tdgchart.getScriptPath().split('/')[1], 'jquery/js/jquery.js'].join('/') ],
-					customScripts = [
-						'lib/d3.v5.min.js',
-						'services/config-service.min.js',
-						'services/utils-service.min.js',
-						'chart/chart.js'
-					];
+				var scripts = [];
+				if (!window.jQuery) {
+					var tdgchartPath = tdgchart.getScriptPath();
+						jqueryPath = tdgchartPath.substr(0, tdgchartPath.indexOf('tdg')) + 'jquery/js/jquery.js';
+						
+					scripts.push(jqueryPath);
+				}
+				
+				var customScripts = [
+					'lib/d3.v5.min.js',
+					'services/config-service.min.js',
+					'services/utils-service.min.js',
+					'chart/chart.js'
+				];
 				
 				return scripts.concat(customScripts);
 			}()),
