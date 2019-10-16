e759f2ad16611a60f0ded09f28eb3aa9afc38544
Mario Delgado <38018368+MD00414@users.noreply.github.com>
2019-10-16 15:59:11 -0400

    VIZ-70

--- com.ibi.sunburst/lib/sunburst.js
+++ com.ibi.sunburst/lib/sunburst.js
@@ -189,6 +189,7 @@ var tdg_sunburst = (function () {
       formatNumber: null,
       isInteractionDisabled: false,
       onRenderComplete: function(){},
+	  customColors: false,   //VIZ-70
       node: {
         colors: ["#4087b8","#e31a1c","#9ebcda","#c994c7","#41b6c4","#49006a","#ec7014","#a6bddb","#67001f","#800026","#addd8e","#e0ecf4","#fcc5c0","#238b45","#081d58","#d4b9da","#2b8cbe","#74a9cf","#41ab5d","#fed976","#ce1256","#7f0000","#a6bddb","#ffffcc","#e7e1ef","#016c59","#f7fcfd","#99d8c9","#fff7fb","#ffffe5","#fdd49e","#ffffd9","#fe9929","#8c96c6","#810f7c","#993404","#c7e9b4","#bfd3e6","#e7298a","#7fcdbb","#3690c0","#ae017e","#d9f0a3","#ece2f0","#014636","#f7fcb9","#66c2a4","#fff7bc","#f7fcf0","#e5f5f9","#fdbb84","#fa9fb5","#4d004b","#fff7fb","#cc4c02","#78c679","#1d91c0","#ccebc5","#feb24c","#b30000","#8c6bb1","#fec44f","#d0d1e6","#084081","#0868ac","#f7fcfd","#0570b0","#ef6548","#fff7ec","#006837","#f768a1","#edf8b1","#fee391","#238443","#ffffe5","#023858","#7a0177","#67a9cf","#dd3497","#980043","#88419d","#d0d1e6","#fc8d59","#4eb3d3","#fd8d3c","#fff7f3","#fc4e2a","#ccece6","#ece7f2","#a8ddb5","#41ae76","#bd0026","#e0f3db","#045a8d","#ffeda0","#253494","#7bccc4","#fde0dd","#00441b","#225ea8","#006d2c","#02818a","#f7f4f9","#d7301f","#df65b0","#662506","#3690c0","#004529","#fee8c8"],
         colorBy: 'evenodd', // 'parent', 'node', 'evenodd'
@@ -443,33 +444,46 @@ var tdg_sunburst = (function () {
         d.ratio = getRatio(d);
       });
  
-      	/*  Logic prior to CHART-2004 NFR
+     /*  Logic prior to CHART-2004 NFR
       var colorScale = ( isEvenOddColorMode() ) 
         ? getEvenOddDiscreteColorScale( sunburstData, innerProps.fakeRootName )
         : getDiscreteColorScale( sunburstData, innerProps.fakeRootName );
 	 */	
+	 
+	  //START VIZ-70	
+	  if (props.customColors){
+		  
+		  var colorScale = ( isEvenOddColorMode() ) 
+			? getEvenOddDiscreteColorScale( sunburstData, innerProps.fakeRootName )
+			: getDiscreteColorScale( sunburstData, innerProps.fakeRootName );	
+			
+	  } //if	
 	  
-	  //Start logic for CHART-2004 NFR. Parent Child color scheme similar to https://codepen.io/johnwun/pen/htJEn and  //https://bl.ocks.org/mbostock/4348373	  
-			  var newColor = d3.scale.category20b();	//https://github.com/d3/d3-3.x-api-reference/blob/master/Ordinal-Scales.md#category20b
-			  sunburstData.forEach(function (d) {
-										function fnShiftColor(color) {
-															 var hsl = d3.rgb(color.brighter(.1)).hsl();
-															 var rnd = Math.round((Math.random() + 1) * 40);
-															 var colorshift = Math.min(hsl.h + rnd,360);
-															 return d3.hsl(colorshift, Math.min(hsl.s,50), Math.min(hsl.l,40));
-														}
-										
-										if (d.depth == 0 ) {
-											d.fillColor = "#ffffff";     //White	
-										}
-										else if (d.depth == 1) {
-											d.fillColor = d3.hsl(newColor(d.name));  //Assign a new color to roots
-										}
-										else {
-											d.fillColor =  fnShiftColor(d.parent.fillColor);  //shiftcolors of children
-										}
-									} );
-	  //End logic for CHART-2004 NFR
+	  else {
+		  //Start logic for CHART-2004 NFR. Parent Child color scheme similar to https://codepen.io/johnwun/pen/htJEn and  //https://bl.ocks.org/mbostock/4348373	  
+				  var newColor = d3.scale.category20b();	//https://github.com/d3/d3-3.x-api-reference/blob/master/Ordinal-Scales.md#category20b
+				  sunburstData.forEach(function (d) {
+											function fnShiftColor(color) {
+																 var hsl = d3.rgb(color.brighter(.1)).hsl();
+																 var rnd = Math.round((Math.random() + 1) * 40);
+																 var colorshift = Math.min(hsl.h + rnd,360);
+																 return d3.hsl(colorshift, Math.min(hsl.s,50), Math.min(hsl.l,40));
+															}
+											
+											if (d.depth == 0 ) {
+												d.fillColor = "#ffffff";     //White	
+											}
+											else if (d.depth == 1) {
+												d.fillColor = d3.hsl(newColor(d.name));  //Assign a new color to roots
+											}
+											else {
+												d.fillColor =  fnShiftColor(d.parent.fillColor);  //shiftcolors of children
+											}
+										} );
+		  //End logic for CHART-2004 NFR
+	  
+	  }//else
+	  //End VIZ-70 	
 	  
       var arc = d3.svg.arc()
         .startAngle(function(d) {
@@ -496,8 +510,9 @@ var tdg_sunburst = (function () {
           fill : function (d) {
             return ( d.name !== innerProps.fakeRootName )
             //? colorScale(getGroup(d, innerProps.fakeRootName))     //Logic prior to CHART-2004 NFR. 
-			? d.fillColor                                            //Logic for CHART-2004. Color is pre-calculated
-            : 'none';
+			//? d.fillColor                                            //Logic for CHART-2004. Color is pre-calculated...overriden by VIZ-70
+				? (props.customColors) ? colorScale(getGroup(d, innerProps.fakeRootName)) : d.fillColor                    //Logic for VIZ-70
+				: 'none';
           },
           stroke: props.node.border.color,
           'stroke-width': props.node.border.width,
