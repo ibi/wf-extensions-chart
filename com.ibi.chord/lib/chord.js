238968e12af64d92c89427618f912ef396a556d7
Sandy Chiang <rjiang@live.com>
2020-10-08 18:53:55 -0400

    VIZ-406

--- com.ibi.chord/lib/chord.js
+++ com.ibi.chord/lib/chord.js
@@ -43,7 +43,9 @@ var com_tdg_chord = (function () {
 			toolTipEnabled: false,
 			//groupColors: ["#4087b8", "#e31a1c", "#9ebcda", "#c994c7", "#41b6c4", "#49006a", "#ec7014", "#a6bddb", "#67001f", "#800026", "#addd8e", "#e0ecf4", "#fcc5c0", "#238b45", "#081d58", "#d4b9da", "#2b8cbe", "#74a9cf", "#41ab5d", "#fed976", "#ce1256", "#7f0000", "#a6bddb", "#ffffcc", "#e7e1ef", "#016c59", "#f7fcfd", "#99d8c9", "#fff7fb", "#ffffe5", "#fdd49e", "#ffffd9", "#fe9929", "#8c96c6", "#810f7c", "#993404", "#c7e9b4", "#bfd3e6", "#e7298a", "#7fcdbb", "#3690c0", "#ae017e", "#d9f0a3", "#ece2f0", "#014636", "#f7fcb9", "#66c2a4", "#fff7bc", "#f7fcf0", "#e5f5f9", "#fdbb84", "#fa9fb5", "#4d004b", "#fff7fb", "#cc4c02", "#78c679", "#1d91c0", "#ccebc5", "#feb24c", "#b30000", "#8c6bb1", "#fec44f", "#d0d1e6", "#084081", "#0868ac", "#f7fcfd", "#0570b0", "#ef6548", "#fff7ec", "#006837", "#f768a1", "#edf8b1", "#fee391", "#238443", "#ffffe5", "#023858", "#7a0177", "#67a9cf", "#dd3497", "#980043", "#88419d", "#d0d1e6", "#fc8d59", "#4eb3d3", "#fd8d3c", "#fff7f3", "#fc4e2a", "#ccece6", "#ece7f2", "#a8ddb5", "#41ae76", "#bd0026", "#e0f3db", "#045a8d", "#ffeda0", "#253494", "#7bccc4", "#fde0dd", "#00441b", "#225ea8", "#006d2c", "#02818a", "#f7f4f9", "#d7301f", "#df65b0", "#662506", "#3690c0", "#004529", "#fee8c8"]
 			//VIZ-399
-			groupColors: user_props.chordColors		};
+			groupColors: user_props.chordColors
+			,showTwoWayInfoTooltip: user_props.showTwoWayInfoTooltip //VIZ-406
+		};
 
 		function getOnAllTransitionComplete(cb) {
 			return function (transition) {
@@ -459,9 +461,16 @@ var com_tdg_chord = (function () {
 			}
 
 			if (props.toolTipEnabled) {
-				/* Code prior to CHART-3146
-				chords.attr('tdgtitle', buildChordToolTip(idToIndx, originalMatrix));
-				*/
+				
+
+				//Start VIZ-406 flag that switches between custom two-way tooltip or use stanard 
+				if(props.showTwoWayInfoTooltip)
+				{
+					/* Code prior to CHART-3146 */
+				 chords.attr('tdgtitle', buildChordToolTip(idToIndx, originalMatrix));
+				}
+				else
+				{
 				//Start CHART-3146
 
 				//Per design pattern at: https://github.com/ibi/wf-extensions-chart/blob/master/com.ibi.tutorial/com.ibi.tutorial_tooltips/com.ibi.tutorial_tooltips.js
@@ -486,15 +495,15 @@ var com_tdg_chord = (function () {
 
 					} //for		
 					//End VIZ-97
+					var datum  =props.data[0][renderIndex];
 					// props.renderConfig.modules.tooltip.addDefaultToolTipContent(this, 0, renderIndex, props.data[0][renderIndex]);	VIZ-78 rework				
-					props.tooltip.addDefaultToolTipContent(this, 0, renderIndex, props.data[0][renderIndex], props.data[0]);
+					props.tooltip.addDefaultToolTipContent(this, 0, renderIndex, datum,  props.data[0]);
 
 				});
-
+				
 				//End CHART-3146
-
-
-
+				}
+				//End VIZ-406 flag that switches between custom two-way tooltip or use stanard 
 			}
 		}
 
