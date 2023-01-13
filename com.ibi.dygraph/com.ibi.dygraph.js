2dcda62adc4b630e88b4491191f372b87dee3ea4
Jan Slupski <jslupski@tibco.com>
2023-01-13 17:13:03 +0100

    [VIZ-771] Property for legend position

--- com.ibi.dygraph/com.ibi.dygraph.js
+++ com.ibi.dygraph/com.ibi.dygraph.js
@@ -45,82 +45,81 @@
 	function noDataRenderCallback(renderConfig) {
 		var chart = renderConfig.moonbeamInstance;
 		var data = [{"timeline":"19960101","value":[1900441,1843626],"_s":0,"_g":0},
-                    {"timeline":"19960201","value":[1982200,1942231],"_s":0,"_g":1},
-                    {"timeline":"19960301","value":[2075663,2018588],"_s":0,"_g":2},
-                    {"timeline":"19960401","value":[1904775,1849614],"_s":0,"_g":3},
-                    {"timeline":"19960501","value":[1877405,1822285],"_s":0,"_g":4},
-                    {"timeline":"19960601","value":[1905163,1893727],"_s":0,"_g":5},
-                    {"timeline":"19960701","value":[1995861,2028306],"_s":0,"_g":6},
-                    {"timeline":"19960801","value":[2018088,2000008],"_s":0,"_g":7},
-                    {"timeline":"19960901","value":[1933779,1932576],"_s":0,"_g":8},
-                    {"timeline":"19961001","value":[1896704,1901828],"_s":0,"_g":9},
-                    {"timeline":"19961101","value":[1907071,1991235],"_s":0,"_g":10},
-                    {"timeline":"19961201","value":[1905152,1894204],"_s":0,"_g":11},
-                    {"timeline":"19970101","value":[1864129,1849988],"_s":0,"_g":12},
-                    {"timeline":"19970201","value":[1861639,1877134],"_s":0,"_g":13},
-                    {"timeline":"19970301","value":[1874439,1901347],"_s":0,"_g":14},
-                    {"timeline":"19970401","value":[1829838,1896457],"_s":0,"_g":15},
-                    {"timeline":"19970501","value":[1899494,1960738],"_s":0,"_g":16},
-                    {"timeline":"19970601","value":[1932630,1925690],"_s":0,"_g":17},
-                    {"timeline":"19970701","value":[2005402,2051937],"_s":0,"_g":18},
-                    {"timeline":"19970801","value":[1838863,1852010],"_s":0,"_g":19},
-                    {"timeline":"19970901","value":[1893944,1955644],"_s":0,"_g":20},
-                    {"timeline":"19971001","value":[1933705,1888870],"_s":0,"_g":21},
-                    {"timeline":"19971101","value":[1865982,1896118],"_s":0,"_g":22},
-                    {"timeline":"19971201","value":[2053923,2046617],"_s":0,"_g":23}];
+			{"timeline":"19960201","value":[1982200,1942231],"_s":0,"_g":1},
+			{"timeline":"19960301","value":[2075663,2018588],"_s":0,"_g":2},
+			{"timeline":"19960401","value":[1904775,1849614],"_s":0,"_g":3},
+			{"timeline":"19960501","value":[1877405,1822285],"_s":0,"_g":4},
+			{"timeline":"19960601","value":[1905163,1893727],"_s":0,"_g":5},
+			{"timeline":"19960701","value":[1995861,2028306],"_s":0,"_g":6},
+			{"timeline":"19960801","value":[2018088,2000008],"_s":0,"_g":7},
+			{"timeline":"19960901","value":[1933779,1932576],"_s":0,"_g":8},
+			{"timeline":"19961001","value":[1896704,1901828],"_s":0,"_g":9},
+			{"timeline":"19961101","value":[1907071,1991235],"_s":0,"_g":10},
+			{"timeline":"19961201","value":[1905152,1894204],"_s":0,"_g":11},
+			{"timeline":"19970101","value":[1864129,1849988],"_s":0,"_g":12},
+			{"timeline":"19970201","value":[1861639,1877134],"_s":0,"_g":13},
+			{"timeline":"19970301","value":[1874439,1901347],"_s":0,"_g":14},
+			{"timeline":"19970401","value":[1829838,1896457],"_s":0,"_g":15},
+			{"timeline":"19970501","value":[1899494,1960738],"_s":0,"_g":16},
+			{"timeline":"19970601","value":[1932630,1925690],"_s":0,"_g":17},
+			{"timeline":"19970701","value":[2005402,2051937],"_s":0,"_g":18},
+			{"timeline":"19970801","value":[1838863,1852010],"_s":0,"_g":19},
+			{"timeline":"19970901","value":[1893944,1955644],"_s":0,"_g":20},
+			{"timeline":"19971001","value":[1933705,1888870],"_s":0,"_g":21},
+			{"timeline":"19971101","value":[1865982,1896118],"_s":0,"_g":22},
+			{"timeline":"19971201","value":[2053923,2046617],"_s":0,"_g":23}];
 
-        var props = renderConfig.properties;
+		var props = renderConfig.properties;
 
-        var width = renderConfig.width;
-        var height = renderConfig.height;
-
-		var container = d3.select(renderConfig.container)
+		var width = renderConfig.width;
+		var height = renderConfig.height;
+		var container = d3.select(renderConfig.container).append("div")
 			.attr('class', 'com_ibi_dygraph');
-           
-        var arrBuckets = [{"id":"timeline","fields":[{"title":"Date"}]},{"id":"value","fields":[{"title":["Sales","Budget"]}]}];
-  
-        com_ibi_dygraph_drawChart(data,container,width,height,arrBuckets,props,chart);
 
-        appendCoverScreen(container, width, height);
+		var arrBuckets = [{"id":"timeline","fields":[{"title":"Date"}]},{"id":"value","fields":[{"title":["Sales","Budget"]}]}];
 
-//		renderConfig.renderComplete();
+		com_ibi_dygraph_drawChart(data,container,width,height,container,width,height,arrBuckets,props,chart);
+
+		appendCoverScreen(container, width, height);
+
+		//		renderConfig.renderComplete();
 	}
 
-    function appendCoverScreen(container, width, height) {
-        container.append("span")
-            .styles({
-                'position': 'absolute',
-                'top': '0px',
-                'left': '0px',
-                'width': width + 'px',
-                'height': height + 'px',
-                'background-color': 'rgba(80,80,80,0.4)',
+	function appendCoverScreen(container, width, height) {
+		container.append("span")
+			.styles({
+				'position': 'absolute',
+				'top': '0px',
+				'left': '0px',
+				'width': width + 'px',
+				'height': height + 'px',
+				'background-color': 'rgba(80,80,80,0.4)',
 				'overflow': 'hidden'
-            });
+			});
 
-        container.append('span')
-            .html('Currently, there are insufficient buckets completed.')
-            .styles({
-                'position': 'absolute',
-                'top': ((height / 2) - 30) + 'px',
-                'left': '0px',
-                'width': width + 'px',
-                'text-align': 'center',
-                'font-weight': 'bold',
-                'font-size': '14px'
-            });
-        container.append('text')
-            .html('Please add more measures or dimensions')
-            .styles({
-                'position': 'absolute',
-                'top': ((height / 2) + 30) + 'px',
-                'left': '0px',
-                'width': width + 'px',
-                'text-align': 'center',
-                'font-weight': 'bold',
-                'font-size': '14px'
-            });
-    }
+		container.append('span')
+			.html('Currently, there are insufficient buckets completed.')
+			.styles({
+				'position': 'absolute',
+				'top': ((height / 2) - 30) + 'px',
+				'left': '0px',
+				'width': width + 'px',
+				'text-align': 'center',
+				'font-weight': 'bold',
+				'font-size': '14px'
+			});
+		container.append('text')
+			.html('Please add more measures or dimensions')
+			.styles({
+				'position': 'absolute',
+				'top': ((height / 2) + 30) + 'px',
+				'left': '0px',
+				'width': width + 'px',
+				'text-align': 'center',
+				'font-weight': 'bold',
+				'font-size': '14px'
+			});
+	}
 
 	// Optional: if defined, is invoked once at the very beginning of each chart engine draw cycle
 	// Use this to configure a specific chart engine instance before rendering.
@@ -134,11 +133,24 @@
 
 		// Example of using the chart engine's built in title properties
 		chart.title.visible = false;
-//		chart.title.text = info.custom_title;
-//		chart.title.text = 'Cool Visualisation!';
+		//		chart.title.text = info.custom_title;
+		//		chart.title.text = 'Cool Visualisation!';
 		chart.footnote.visible = false;
 		chart.footnote.text = 'xxxxxxxx';
 		chart.footnote.align = 'right';
+	}
+
+	function getLegendMaxWidth(data, font) {
+		var maxval = Math.ceil(tdgchart.util.max(data,"value")) + ".99"; //default dwo decimal digits
+		// a little dumb algorithm looking for the longest string. But measuring each string would be costly
+		var labelLengths = data.map(function (a) { return typeof a.color == 'string' ? { s: a.color, l: a.color.length} : { s:'', l:0 };});
+		var longestLabel = labelLengths.reduce(function (t,v) { return t.l >= v.l ? t : v;}, { s:'', l:0 });
+		var valWidth = tdgchart.util.measureLabelWidth(maxval, font);
+		var labelWidth = tdgchart.util.measureLabelWidth(longestLabel.s + ': ', font);
+		var emWidth = tdgchart.util.measureLabelWidth('EM', font);
+
+		//legend width is either label + marker or label + value
+		return Math.max(emWidth, valWidth) + labelWidth + 1;
 	}
 
 	// Required: Invoked during each chart engine draw cycle
@@ -154,48 +166,74 @@
 		var chart = renderConfig.moonbeamInstance;
 		var data = renderConfig.data;
 		var props = renderConfig.properties;
-        
-        if (Array.isArray(renderConfig.dataBuckets.buckets)) {
-            var arrBuckets = renderConfig.dataBuckets.buckets;
-        } else {
-            var arrBuckets = [];
-            if (renderConfig.dataBuckets.buckets.timeline) {
-                var arrTimeline = []
-                arrTimeline.push({
-                    title: renderConfig.dataBuckets.buckets.timeline.title
-                })
-                arrBuckets.push({
-                    id: "timeline",
-                    fields: arrTimeline
-                })
-            }
-            if (renderConfig.dataBuckets.buckets.color) {
-                arrBuckets.push({
-                    id: "color",
-                    fields: renderConfig.dataBuckets.buckets.color
-                })
-            }
-            if (renderConfig.dataBuckets.buckets.value) {
-                var arrValues = []
-                for (x = 0; x < renderConfig.dataBuckets.buckets.value.title.length; x++) {
-                    arrValues.push({
-                        title: renderConfig.dataBuckets.buckets.value.title[x]
-                    })
-                }
-                arrBuckets.push({
-                    id: "value",
-                    fields: arrValues
-                })
-            }
-        }
-  
-        var width = renderConfig.width;
-        var height = renderConfig.height;
 
-		var container = d3.select(renderConfig.container)
-			.attr('class', 'com_ibi_dygraph');
-            
-		com_ibi_dygraph_drawChart(data,container,width,height,arrBuckets,props,chart);
+		if (Array.isArray(renderConfig.dataBuckets.buckets)) {
+			var arrBuckets = renderConfig.dataBuckets.buckets;
+		} else {
+			var arrBuckets = [];
+			if (renderConfig.dataBuckets.buckets.timeline) {
+				var arrTimeline = []
+				arrTimeline.push({
+					title: renderConfig.dataBuckets.buckets.timeline.title
+				})
+				arrBuckets.push({
+					id: "timeline",
+					fields: arrTimeline
+				})
+			}
+			if (renderConfig.dataBuckets.buckets.color) {
+				arrBuckets.push({
+					id: "color",
+					fields: renderConfig.dataBuckets.buckets.color
+				})
+			}
+			if (renderConfig.dataBuckets.buckets.value) {
+				var arrValues = []
+				for (x = 0; x < renderConfig.dataBuckets.buckets.value.title.length; x++) {
+					arrValues.push({
+						title: renderConfig.dataBuckets.buckets.value.title[x]
+					})
+				}
+				arrBuckets.push({
+					id: "value",
+					fields: arrValues
+				})
+			}
+		}
+
+		var width = renderConfig.width;
+		var height = renderConfig.height;
+
+		var masterContainer = d3.select(renderConfig.container);
+		var legendWidth = 0;
+
+		if (props.legend === "never" || props.legendPosition != 'left' && props.legendPosition != 'right') {
+			//legend overlaid - default (dygraph will put legend in masterContainer if props.labelsDiv not set)
+			chartContainer = masterContainer;
+		} else {
+			masterContainer.style('display', 'flex');
+
+			var chartContainer, legendContainer;
+			if (props.legendPosition == 'right') { //order of 'appends' matters
+				chartContainer = masterContainer.append("div");
+				legendContainer = masterContainer.append("div");
+			} else {
+				legendContainer = masterContainer.append("div");
+				chartContainer = masterContainer.append("div");
+			}
+			chartContainer.attr('id', masterContainer.attr('id') + '-chart');
+
+			legendContainer.attr('id', masterContainer.attr('id') + '-legend');
+			legendContainer.style('flex-grow', '1');
+
+			var font = '700 ' + window.getComputedStyle(renderConfig.container).font; // "700" is for "bold"
+			legendWidth = Math.min(width*0.3, getLegendMaxWidth(data, font)); // limit legend width to 30% of available space
+			props.labelsDiv = legendContainer.node(0); //this enables 'external' legend rendering in dygraph engine
+		}
+
+		chartContainer.attr('class', 'com_ibi_dygraph');
+
+		com_ibi_dygraph_drawChart(data, masterContainer, width, height, chartContainer,width-legendWidth,height,arrBuckets,props,chart);
 
 		renderConfig.renderComplete();
 	}
@@ -210,7 +248,7 @@
 		noDataPreRenderCallback: noDataPreRenderCallback,
 		noDataRenderCallback: noDataRenderCallback,
 		resources: {  // Additional external resources (CSS & JS) required by this extension
-			script: window.d3 ? ['lib/dygraph.min.js','lib/dychart.js','lib/d3-selection-multi.min.js','lib/d3-time.min.js','lib/d3-time-format.min.js','lib/d3-transition.min.js'] : 
+			script: window.d3 ? ['lib/dygraph.min.js','lib/dychart.js','lib/d3-selection-multi.min.js','lib/d3-time.min.js','lib/d3-time-format.min.js','lib/d3-transition.min.js'] :
 			['lib/dygraph.min.js','lib/d3.v5.16.min.js','lib/dychart.js','lib/d3-selection-multi.min.js','lib/d3-time.min.js','lib/d3-time-format.min.js','lib/d3-transition.min.js'],
 			css: ['css/dygraph.css']
 		},
@@ -230,18 +268,18 @@
 				supported: false,  // Set this true if your extension wants to enable HTML tooltips
 			}
 			// Not used in this extension; here for documentation purposes.
-//			colorScale: {
-//				supported: true,
-//				minMax: function(arg){}  // Optional: return a {min, max} object to use for the color scale min & max.
-//			}
-//			sizeScale: {
-//				supported: false,
-//				minMax: function(arg){}  // Optional: return a {min, max} object to use for the size scale min & max.
-//			},
-//			legend: {
-//				colorMode: function(arg){}, // Return either 'data' or 'series'.  If implemented, force the chart engine to use this color mode legend
-//				sizeMode: function(arg){},  // return either 'size' or falsey.  If implemented, force the chart engine to use this size legend
-//			}
+			//			colorScale: {
+			//				supported: true,
+			//				minMax: function(arg){}  // Optional: return a {min, max} object to use for the color scale min & max.
+			//			}
+			//			sizeScale: {
+			//				supported: false,
+			//				minMax: function(arg){}  // Optional: return a {min, max} object to use for the size scale min & max.
+			//			},
+			//			legend: {
+			//				colorMode: function(arg){}, // Return either 'data' or 'series'.  If implemented, force the chart engine to use this color mode legend
+			//				sizeMode: function(arg){},  // return either 'size' or falsey.  If implemented, force the chart engine to use this size legend
+			//			}
 		}
 	};
 
--- com.ibi.dygraph/lib/dychart.js
+++ com.ibi.dygraph/lib/dychart.js
@@ -13,118 +13,123 @@
 // --------------------------------------------------------------------------------
 
 var com_ibi_dygraph_mygraph = null;
-function com_ibi_dygraph_drawChart(data,Container,width,height,arrBuckets,props,chart) {
-
-// Older versions (8.2.01 for one) do not pass the newer array style of arrBuckets,
-// so we need to test and adjust accordingly
-  if (Array.isArray(arrBuckets)) {
-      var csvfile = arrBuckets[0].fields[0].title;
-	  // for this extension, customBars cannot be true without errorBars also being true.
-	  props.errorBars = props.customBars ? props.customBars : props.errorBars;
-// If the new color bucket is set then the data needs to be rearranged into one row per color bucket value
-// This section writes all the color bucket values into the csv file header 
-	  if (arrBuckets[1].id === "color") {
-		  console.log("Color bucket exists so rearrange the data");
-		  var colorArray = d3.map(data, function(d,i) {return d.color}).keys();
-		  if (arrBuckets[2].fields.length > 1) {
-			for (x = 0; x < colorArray.length; x++) {
-			  for (y=0 ; y < arrBuckets[2].fields.length; y++) {
-				csvfile += ", " + colorArray[x] + " (" + arrBuckets[2].fields[y].title + ")";
-			  }
-			}
-		  } else {
-			csvfile += ", " + colorArray;
-		  }
-	  } else {
-	  // when errorBars is set to true, the labels passed into the csvfile need to be
-	  // manipulated to allow for unlabelled error ranges to be arrayed.
-	  // otherwise errors are returned.
-		  for (x = 0; x < arrBuckets[1].fields.length; x++) {
-			csvfile += ", " + arrBuckets[1].fields[x].title;
-			if (props.errorBars && props.customBars) {
-				x = x + 2
-			} else {
-				if (props.errorBars) {
-					x = x + 1;
-				} else {
-					if (props.customBars) {
-						x = arrBuckets[1].fields.length + 1;
+function com_ibi_dygraph_drawChart(data,rootContainer,rootWidth, rootHeight,chartContainer,width,height,arrBuckets,props,chart) {
+	// Older versions (8.2.01 for one) do not pass the newer array style of arrBuckets,
+	// so we need to test and adjust accordingly
+	if (Array.isArray(arrBuckets)) {
+		var csvfile = arrBuckets[0].fields[0].title;
+		// for this extension, customBars cannot be true without errorBars also being true.
+		props.errorBars = props.customBars ? props.customBars : props.errorBars;
+		// If the new color bucket is set then the data needs to be rearranged into one row per color bucket value
+		// This section writes all the color bucket values into the csv file header 
+		if (arrBuckets[1].id === "color") {
+			console.log("Color bucket exists so rearrange the data");
+			var colorArray = d3.map(data, function(d,i) {return d.color}).keys();
+			if (arrBuckets[2].fields.length > 1) {
+				for (x = 0; x < colorArray.length; x++) {
+					for (y=0 ; y < arrBuckets[2].fields.length; y++) {
+						csvfile += ", " + colorArray[x] + " (" + arrBuckets[2].fields[y].title + ")";
 					}
 				}
-			};
+			} else {
+				csvfile += ", " + colorArray;
+			}
+		} else {
+			// when errorBars is set to true, the labels passed into the csvfile need to be
+			// manipulated to allow for unlabelled error ranges to be arrayed.
+			// otherwise errors are returned.
+			for (x = 0; x < arrBuckets[1].fields.length; x++) {
+				csvfile += ", " + arrBuckets[1].fields[x].title;
+				if (props.errorBars && props.customBars) {
+					x = x + 2
+				} else {
+					if (props.errorBars) {
+						x = x + 1;
+					} else {
+						if (props.customBars) {
+							x = arrBuckets[1].fields.length + 1;
+						}
+					}
+				};
+			}
 		}
-    }
-  } else {
-      var csvfile = arrBuckets.timeline.title + "," + arrBuckets.value.title;
-  }
+	} else {
+		var csvfile = arrBuckets.timeline.title + "," + arrBuckets.value.title;
+	}
 
-// If the new color bucket is set then the data needs to be rearranged into one row per color bucket value
-// This section wraps the data into the correct orientation when this is true 
-  if (arrBuckets[1].id === "color") {
-	  var dataValues = d3.map(data, function(d,i) {
-		  // For each unique value, write the data onto the next line of csv file
-		  // If d.value is an Array the concatenation will work using the same method
-		  if ((i / colorArray.length) == parseInt(i / colorArray.length)) {
-			  csvfile += "\n" + d.timeline + "," + d.value;
-		  } else {
-			  csvfile += "," + d.value;
-		  }
-		  return d.timeline + "," + d.value;
+	// If the new color bucket is set then the data needs to be rearranged into one row per color bucket value
+	// This section wraps the data into the correct orientation when this is true 
+	if (arrBuckets[1].id === "color") {
+		var dataValues = d3.map(data, function(d,i) {
+			// For each unique value, write the data onto the next line of csv file
+			// If d.value is an Array the concatenation will work using the same method
+			if ((i / colorArray.length) == parseInt(i / colorArray.length)) {
+				csvfile += "\n" + d.timeline + "," + d.value;
+			} else {
+				csvfile += "," + d.value;
+			}
+			return d.timeline + "," + d.value;
 		});
-  } else {
-	// If the color bucket isn't used then write out csv file as before
-	  var dataValues = d3.map(data, function(d,i) {
+	} else {
+		// If the color bucket isn't used then write out csv file as before
+		var dataValues = d3.map(data, function(d,i) {
 			csvfile += '\n' + d.timeline + "," + d.value;
-            return d.timeline + "," + d.value;
+			return d.timeline + "," + d.value;
 		});
-  }
+	}
 
-  var chartDiv = document.getElementById(Container.node(0).id)
-  //var chartDiv = document.getElementById(Container[0][0].id);
-  var win = window
-    , doc = document
-    , ele = doc.documentElement
-    , bdy = doc.getElementsByTagName("body")[0]
-    , screenWidth = screen.width;
+	var win = window
+		, doc = document
+		, ele = doc.documentElement
+		, bdy = doc.getElementsByTagName("body")[0]
+		, screenWidth = screen.width;
 
-// Allow the chart title defined within Moonbeam to override that set in the properties
-// This will allow multigraph settings to provide a data related value rather than the same throughout.
-  props.title = chart.title.text === '' ? props.title : chart.title.text;
-  
-// Some properties are not aligned with the area of interest such as xLabelHeight and yLabelWidth
-// so they have been "realigned" and need to be reassigned here
-  props.xLabelHeight = props.axes.x.axisLabelHeight;
-  props.xlabel = props.axes.x.axisLabel;
-  props.yLabelWidth = props.axes.y.axisLabelHeight;
-  props.ylabel = props.axes.y.axisLabel;
-// To allow selection of colors, the array is split into separate attributes so we need to
-// merge them back into an array.
-  props.colors = [props.colours.color0, props.colours.color1, props.colours.color2, props.colours.color3, props.colours.color4, props.colours.color5, props.colours.color6, props.colours.color7, props.colours.color8, props.colours.color9];
+	// Allow the chart title defined within Moonbeam to override that set in the properties
+	// This will allow multigraph settings to provide a data related value rather than the same throughout.
+	props.title = chart.title.text === '' ? props.title : chart.title.text;
 
-  var main_margin = {top: 15, right: 15, bottom: 15, left: 15};
+	// Some properties are not aligned with the area of interest such as xLabelHeight and yLabelWidth
+	// so they have been "realigned" and need to be reassigned here
+	props.xLabelHeight = props.axes.x.axisLabelHeight;
+	props.xlabel = props.axes.x.axisLabel;
+	props.yLabelWidth = props.axes.y.axisLabelHeight;
+	props.ylabel = props.axes.y.axisLabel;
+	// To allow selection of colors, the array is split into separate attributes so we need to
+	// merge them back into an array.
+	props.colors = [props.colours.color0, props.colours.color1, props.colours.color2, props.colours.color3, props.colours.color4, props.colours.color5, props.colours.color6, props.colours.color7, props.colours.color8, props.colours.color9];
 
-  chartDiv.style.top = main_margin.top + "px";
-  chartDiv.style.left = main_margin.left + "px";
-  chartDiv.style.width = width - main_margin.left - main_margin.right + "px";
-  chartDiv.style.height = height - main_margin.top - main_margin.bottom + "px";
+	var main_margin = {top: 15, right: 15, bottom: 15, left: 15};
 
-//BEGIN VIZ-378
-if(com_ibi_dygraph_mygraph == null)
-{
-  com_ibi_dygraph_mygraph = new Dygraph(
-	  chartDiv,
-	  csvfile, // CSV file
-	  props
-	);
-}
-else
-{
-  com_ibi_dygraph_mygraph.destroy();
-  com_ibi_dygraph_mygraph = new Dygraph(
-	  chartDiv,
-	  csvfile, // CSV file
-	  props
-	);
-}
-//END VIZ-378
+	var rootDivStyle = rootContainer.node(0).style;
+	var chartDiv = chartContainer.node(0);
+	rootDivStyle.top = "0";
+	rootDivStyle.left = "0";
+	rootDivStyle.marginTop = main_margin.top + "px";
+	rootDivStyle.marginLeft = main_margin.left + "px";
+	rootDivStyle.marginBottom = main_margin.bottom + "px";
+	rootDivStyle.marginRight = main_margin.right + "px";
+	rootDivStyle.width = rootWidth - main_margin.left - main_margin.right + "px";
+	rootDivStyle.height = rootHeight - main_margin.top - main_margin.bottom + "px";
+	chartDiv.style.width = width - main_margin.left - main_margin.right + "px";
+	chartDiv.style.height = height - main_margin.top - main_margin.bottom + "px";
+
+	//BEGIN VIZ-378
+	if(com_ibi_dygraph_mygraph == null)
+	{
+		com_ibi_dygraph_mygraph = new Dygraph(
+			chartDiv,
+			csvfile, // CSV file
+			props
+		);
+	}
+	else
+	{
+		com_ibi_dygraph_mygraph.destroy();
+		com_ibi_dygraph_mygraph = new Dygraph(
+			chartDiv,
+			csvfile, // CSV file
+			props
+		);
+	}
+	//END VIZ-378
 }
